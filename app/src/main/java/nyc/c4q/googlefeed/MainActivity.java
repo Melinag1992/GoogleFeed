f2ca4ef9fe02e4092bb27febe0c03c7ff1340116
Yoki Lam <yokeyenlam@ac.c4q.nyc>
2017-12-20 21:16:15 -0500

    changed something

--- app/src/main/java/nyc/c4q/googlefeed/MainActivity.java
+++ app/src/main/java/nyc/c4q/googlefeed/MainActivity.java
@@ -56,9 +56,9 @@ public class MainActivity extends AppCompatActivity {
     private String articleImage;
     private String articleDescription;
     private String articleTitle;
-    private static final String TAG= MainActivity.class.getSimpleName();
-    private final static String API_KEY= "f3887b19f7bec24ad815dde137f8f6a1";
-    private List<Movie> currentMovies= new ArrayList<>();
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private final static String API_KEY = "f3887b19f7bec24ad815dde137f8f6a1";
+    private List <Movie> currentMovies = new ArrayList <>();
     private RecyclerView rv;
 
     private static final String WEATHER_API_KEY = "d5730a368a881b4061f35adf65c2da29";
@@ -78,43 +78,42 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please check your API KEY", Toast.LENGTH_LONG).show();
         }
 
-        rv= findViewById(R.id.upcoming_rv);
+        rv = findViewById(R.id.upcoming_rv);
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         getRetrofit();
 
     }
 
-    public void views(){
-        sv= findViewById(R.id.sv);
+    public void views() {
+        sv = findViewById(R.id.sv);
         displayWeather = findViewById(R.id.data);
         dateDisplay = findViewById(R.id.date);
         displayTemp = findViewById(R.id.min_temp);
         titleTextview = findViewById(R.id.titletext);
-        displayIcon= findViewById(R.id.weather_image);
+        displayIcon = findViewById(R.id.weather_image);
         search = findViewById(R.id.search_bar);
-        movie= findViewById(R.id.movieButton);
-        verge= findViewById(R.id.vergeButton);
+        movie = findViewById(R.id.movieButton);
+        verge = findViewById(R.id.vergeButton);
         titleTextview = findViewById(R.id.titletext);
         // description = findViewById(R.id.description_textView);
         newsImage = findViewById(R.id.imageView);
-        movieCard= findViewById(R.id.movie_card);
-        vergeCard= findViewById(R.id.verge_cardview);
-//        movie.scrollTo(0, (int)displayIcon.getY());
+        movieCard = findViewById(R.id.movie_card);
+        vergeCard = findViewById(R.id.verge_cardview);
     }
 
     private void setScroll() {
         movie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sv.smoothScrollTo(0, (int)movieCard.getY());
-                Log.d(TAG, "onClick: "+ movieCard.getX() +" " + (int)movieCard.getY());
+                sv.smoothScrollTo(0, (int) movieCard.getY());
+                Log.d(TAG, "onClick: " + movieCard.getX() + " " + (int) movieCard.getY());
             }
         });
         verge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sv.smoothScrollTo(0, ((int)vergeCard.getY()-150));
-                Log.d(TAG, "onClick: "+ vergeCard.getX() +" " + (int)vergeCard.getY());
+                sv.smoothScrollTo(0, ((int) vergeCard.getY() - 150));
+                Log.d(TAG, "onClick: " + vergeCard.getX() + " " + (int) vergeCard.getY());
             }
         });
     }
@@ -130,26 +129,21 @@ public class MainActivity extends AppCompatActivity {
 
         WeatherApi weatherservice = retrofit.create(WeatherApi.class);
 
-        Call<GetCurrently> currentlyResponse = weatherservice.getResponse();
-        currentlyResponse.enqueue(new Callback<GetCurrently>() {
+        Call <GetCurrently> currentlyResponse = weatherservice.getResponse();
+        currentlyResponse.enqueue(new Callback <GetCurrently>() {
             @Override
-            public void onResponse(Call<GetCurrently> call, Response<GetCurrently> response) {
+            public void onResponse(Call <GetCurrently> call, Response <GetCurrently> response) {
 
                 long unixTime = response.body().getCurrently().getTime();
                 Date date = new Date(unixTime * 1000L);
-
                 Log.e(TAG, "onResponse: " + date);
-
                 DateFormat dateformat = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.ENGLISH);
-
                 Log.e(TAG, "onResponse: " + dateformat);
-
                 dateformat.setTimeZone(TimeZone.getTimeZone("EST"));
                 String format3 = dateformat.format(date);
                 Log.e(TAG, "onResponse: " + format3);
 
                 Currently currently = response.body().getCurrently();
-
                 String temp = String.valueOf((int) currently.getTemperature()) + "\u2109\n";
 
                 result = "Summary: " + currently.getSummary() + "\n" +
@@ -161,16 +155,14 @@ public class MainActivity extends AppCompatActivity {
                 displayTemp.setText(temp);
                 dateDisplay.setText(format3);
                 displayWeather.setText(result);
-
             }
 
             @Override
-            public void onFailure(Call<GetCurrently> call, Throwable t) {
-                Log.d(TAG, "onFailure: "+ t.toString());
+            public void onFailure(Call <GetCurrently> call, Throwable t) {
+                Log.d(TAG, "onFailure: " + t.toString());
             }
         });
     }
-
 
     public void buzzfeedApi() {
 
@@ -183,35 +175,35 @@ public class MainActivity extends AppCompatActivity {
 
         BuzzfeedfeedApi buzzfeedtopStories = buzzFeedRetroFit.create(BuzzfeedfeedApi.class);
 
-        Call<VergeReponse> vergeArticleResponse = buzzfeedtopStories.getVergeArticlesResponse("the-next-web,the-verge", "86df0f155fd140709ce109d2f7555cb5");
-        vergeArticleResponse.enqueue(new Callback<VergeReponse>() {
+        Call <VergeReponse> vergeArticleResponse = buzzfeedtopStories.getVergeArticlesResponse("the-next-web,the-verge", "86df0f155fd140709ce109d2f7555cb5");
+        vergeArticleResponse.enqueue(new Callback <VergeReponse>() {
             @Override
-            public void onResponse(Call<VergeReponse> call, Response<VergeReponse> response) {
+            public void onResponse(Call <VergeReponse> call, Response <VergeReponse> response) {
 
-               articleTitle =  response.body().getArticle().get(0).getTitle();
-             //  articleDescription = response.body().getArticle().get(0).getDescription();
-               articleImage = response.body().getArticle().get(0).getUrltoimage();
+                articleTitle = response.body().getArticle().get(0).getTitle();
+                //  articleDescription = response.body().getArticle().get(0).getDescription();
+                articleImage = response.body().getArticle().get(0).getUrltoimage();
 
-                    titleTextview.setText(articleTitle);
-                   // description.setText(articleDescription);
-                    Picasso.with(getApplicationContext())
+                titleTextview.setText(articleTitle);
+                // description.setText(articleDescription);
+                Picasso.with(getApplicationContext())
                         .load(articleImage)
-                            .resize(400,400)
-                            .centerCrop()
-                            .placeholder(R.drawable.ic_launcher_background)
-                            .error(R.drawable.ic_launcher_background)
+                        .resize(400, 400)
+                        .centerCrop()
+                        .placeholder(R.drawable.ic_launcher_background)
+                        .error(R.drawable.ic_launcher_background)
                         .into(newsImage);
             }
 
             @Override
             public void onFailure(Call <VergeReponse> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t );
+                Log.e(TAG, "onFailure: " + t);
             }
         });
     }
-  
+
     public void getWeatherIcon(String icon) {
-        switch(icon) {
+        switch (icon) {
             case "clear-day":
                 displayIcon.setImageResource(R.drawable.clear_day);
                 break;
@@ -247,36 +239,36 @@ public class MainActivity extends AppCompatActivity {
 
     private void getRetrofit() {
         final String BASE_URL = "http://api.themoviedb.org/3/";
-        Retrofit retrofit= new Retrofit.Builder()
+        Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         MovieService movieService = retrofit.create(MovieService.class);
 
-        final Call<MovieResponse> currentM= movieService.getNowPlayingMovies(API_KEY);
+        final Call <MovieResponse> currentM = movieService.getNowPlayingMovies(API_KEY);
         currentM.enqueue(new Callback <MovieResponse>() {
             @Override
-            public void onResponse(Call <MovieResponse> call, Response<MovieResponse> response) {
-                currentMovies=response.body().getResults();
+            public void onResponse(Call <MovieResponse> call, Response <MovieResponse> response) {
+                currentMovies = response.body().getResults();
                 rv.setAdapter(new upComigMoviesAdapter(currentMovies));
             }
 
             @Override
             public void onFailure(Call <MovieResponse> call, Throwable t) {
-                Log.d(TAG, "onFailure: "+ t.toString());
+                Log.d(TAG, "onFailure: " + t.toString());
             }
         });
     }
 
-    public void search(){
+    public void search() {
         search.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if(event.getAction() == event.ACTION_DOWN ) {
-                    if((keyCode == event.KEYCODE_ENTER || keyCode == event.KEYCODE_TAB)&& !TextUtils.isEmpty(search.getText().toString())){
-                        String searchText= search.getText().toString();
-                        Intent intent= new Intent(Intent.ACTION_VIEW);
+                if (event.getAction() == event.ACTION_DOWN) {
+                    if ((keyCode == event.KEYCODE_ENTER || keyCode == event.KEYCODE_TAB) && !TextUtils.isEmpty(search.getText().toString())) {
+                        String searchText = search.getText().toString();
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
                         intent.setData(Uri.parse("https://www.google.com/search?q=" + searchText));
                         startActivity(intent);
                         search.setText("");
